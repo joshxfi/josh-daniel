e126080c932f1fb46ee82e8ab3781eb852c17892
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-23 10:20:31 +0800

    feat: add homepage

---
+++ src/components/icons.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export function ArrowUpRightIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 20 20"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
