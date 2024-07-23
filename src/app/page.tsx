d23d7fe9129f46efd9d0f9d2f6049753b8bfc59f
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-23 12:35:41 +0800

    chore: add url for linkedin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Projects } from "@/components/projects";
-import { ArrowUpRightIcon } from "@/components/icons";
 import { DotPattern } from "@/components/dot-pattern";
+import { ArrowUpRightIcon, LinkedInIcon } from "@/components/icons";
 
 export default function Home() {
   return (
     <main className="mx-auto pt-24 max-w-screen-sm container relative">
-      <h1 className="font-semibold text-white">Josh Daniel</h1>
+      <div className="flex items-center gap-2">
+        <h1 className="font-semibold text-white">Josh Daniel</h1>
+
+        <Link
+          href="https://www.linkedin.com/in/joshxfi/"
+          target="_blank"
+          className="text-zinc-200 hover:text-white transition-colors"
+        >
+          <LinkedInIcon className="size-4" />
+        </Link>
+      </div>
 
       <p className="mt-6">
         Hello, I'm a software developer based in the Philippines. I specialize
