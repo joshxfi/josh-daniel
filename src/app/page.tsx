31427f8a3adabbe6e42e73b9440ea428208fdf48
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2026-03-14 19:35:17 +0800

    refactor: upgrade to tailwind v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { ArrowUpRightIcon, LinkedInIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <main className="mx-auto md:pt-24 pt-16 max-w-screen-sm container relative">
+    <main className="mx-auto md:pt-24 pt-16 max-w-(--breakpoint-sm) container relative">
       <div className="flex items-center gap-2">
         <h1 className="font-semibold text-white">Josh Daniel</h1>
 
@@ -53,7 +53,7 @@ export default function Home() {
 
       <DotPattern
         className={cn(
-          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)] md:mt-36 mt-24",
+          "mask-[radial-gradient(300px_circle_at_center,white,transparent)] md:mt-36 mt-24",
         )}
       />
     </main>
